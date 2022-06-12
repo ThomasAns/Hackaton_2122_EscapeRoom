f5323014f45e86861a9a2bfbc0fde757c7e51d31
ThomasAns <83822439+ThomasAns@users.noreply.github.com>
2022-06-12 21:11:24 +0200

    Site bug fixes responsive

--- js/scribbler.js
+++ js/scribbler.js
@@ -1,18 +1,19 @@
 // utilities
-var get = function (selector, scope) {
+const get = function (selector, scope) {
   scope = scope ? scope : document;
   return scope.querySelector(selector);
 };
 
-var getAll = function (selector, scope) {
+const getAll = function (selector, scope) {
   scope = scope ? scope : document;
   return scope.querySelectorAll(selector);
 };
 
+let i;
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
-  var i = 0;
-  var txt = `scribbler
+  i = 0;
+  const txt = `scribbler
             [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
 
             ###todo for new year dinner party
@@ -21,7 +22,7 @@ if (document.getElementsByClassName('demo').length > 0) {
             - butter
             - green onion
             - lots and lots of kiwis 🥝`;
-  var speed = 60;
+  const speed = 60;
 
   function typeItOut () {
     if (i < txt.length) {
@@ -37,28 +38,29 @@ if (document.getElementsByClassName('demo').length > 0) {
 // toggle tabs on codeblock
 window.addEventListener("load", function() {
   // get all tab_containers in the document
-  var tabContainers = getAll(".tab__container");
+  const tabContainers = getAll(".tab__container");
 
   // bind click event to each tab container
-  for (var i = 0; i < tabContainers.length; i++) {
+  for (let i = 0; i < tabContainers.length; i++) {
     get('.tab__menu', tabContainers[i]).addEventListener("click", tabClick);
   }
 
   // each click event is scoped to the tab_container
   function tabClick (event) {
-    var scope = event.currentTarget.parentNode;
-    var clickedTab = event.target;
-    var tabs = getAll('.tab', scope);
-    var panes = getAll('.tab__pane', scope);
-    var activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
+    let i;
+    const scope = event.currentTarget.parentNode;
+    const clickedTab = event.target;
+    const tabs = getAll('.tab', scope);
+    const panes = getAll('.tab__pane', scope);
+    const activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
 
     // remove all active tab classes
-    for (var i = 0; i < tabs.length; i++) {
+    for (i = 0; i < tabs.length; i++) {
       tabs[i].classList.remove('active');
     }
 
     // remove all active pane classes
-    for (var i = 0; i < panes.length; i++) {
+    for (i = 0; i < panes.length; i++) {
       panes[i].classList.remove('active');
     }
 
@@ -69,12 +71,12 @@ window.addEventListener("load", function() {
 });
 
 //in page scrolling for documentaiton page
-var btns = getAll('.js-btn');
-var sections = getAll('.js-section');
+const btns = getAll('.js-btn');
+const sections = getAll('.js-section');
 
 function setActiveLink(event) {
   // remove all active tab classes
-  for (var i = 0; i < btns.length; i++) {
+  for (let i = 0; i < btns.length; i++) {
     btns[i].classList.remove('selected');
   }
 
@@ -82,7 +84,7 @@ function setActiveLink(event) {
 }
 
 function smoothScrollTo(i, event) {
-  var element = sections[i];
+  let element = sections[i];
   setActiveLink(event);
 
   window.scrollTo({
@@ -93,17 +95,17 @@ function smoothScrollTo(i, event) {
 }
 
 if (btns.length && sections.length > 0) {
-  for (var i = 0; i<btns.length; i++) {
+  for (i = 0; i<btns.length; i++) {
     btns[i].addEventListener('click', smoothScrollTo.bind(this,i));
   }
 }
 
 // fix menu to page-top once user starts scrolling
 window.addEventListener('scroll', function () {
-  var docNav = get('.doc__nav > ul');
+  const docNav = get('.doc__nav > ul');
 
   if( docNav) {
-    if (window.pageYOffset > 63) {
+    if (window.scrollY > 63) {
       docNav.classList.add('fixed');
     } else {
       docNav.classList.remove('fixed');
@@ -112,8 +114,8 @@ window.addEventListener('scroll', function () {
 });
 
 // responsive navigation
-var topNav = get('.menu');
-var icon = get('.toggle');
+const topNav = get('.menu');
+const icon = get('.toggle');
 
 window.addEventListener('load', function(){
   function showNav() {
